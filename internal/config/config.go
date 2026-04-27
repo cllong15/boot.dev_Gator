dbd25bb6d8846a11c8b4fd45c88fb9b47bf32f97
cllong15 <17620963+cllong15@users.noreply.github.com>
2026-04-27 08:44:23 -0700

    ammended to conform to lesson

--- internal/config/config.go
+++ internal/config/config.go
@@ -6,6 +6,26 @@ import (
 	"os"
 )
 
+const configFileName = ".gatorconfig.json"
+
+type Config struct {
+	DbURL           string `json:"db_url"`
+	CurrentUserName string `json:"current_user_name"`
+}
+
+func SetUser(cfg Config, currentUser string) error {
+	cfg.CurrentUserName = currentUser
+	ok := writeConfig(cfg)
+	if ok != nil {
+		return fmt.Errorf("Config SetUser: %v\n", ok)
+	}
+	return nil
+}
+
+func getConfigFilePath() (string, error) {
+	return "./" + configFileName, nil
+}
+
 func Read() (Config, error) {
 	// read json file
 	cfgFile, ok := getConfigFilePath()
