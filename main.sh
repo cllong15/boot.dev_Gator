1196e8cb5a00076f5c01f5cd9e9aa24aa9273c94
cllong15 <17620963+cllong15@users.noreply.github.com>
2026-05-06 12:00:26 -0700

    update main.sh

--- main.sh
+++ main.sh
@@ -8,19 +8,19 @@ go run . register holgith
 echo "======"
 go run . addfeed "Lanes Blog" "https://www.wagslane.dev/index.xml"
 echo "======"
-go run . addfeed "Boot.dev Blog" "https://www.boot.dev/blog/index.xml"
-echo "======"
-go run . addfeed "TechCrunch" "https://techcrunch.com/feed/"
-echo "======"
-go run . addfeed "Hacker News" "https://news.ycombinator.com/rss"
-echo "======"
+# go run . addfeed "Boot.dev Blog" "https://www.boot.dev/blog/index.xml"
+# echo "======"
+# go run . addfeed "TechCrunch" "https://techcrunch.com/feed/"
+# echo "======"
+# go run . addfeed "Hacker News" "https://news.ycombinator.com/rss"
+# echo "======"
 go run . follow "https://hnrss.org/newest"
 echo "======"
 go run . following
 echo "======"
 go run . login kahya
 echo "======"
-go run . follow "https://news.ycombinator.com/rss"
+# go run . follow "https://news.ycombinator.com/rss"
 echo "======"
 go run . following
 
