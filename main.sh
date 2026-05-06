680c142eafebc389554d700073cabe2a19f69ec9
cllong15 <17620963+cllong15@users.noreply.github.com>
2026-05-05 23:46:21 -0700

    Added middleware for handlers that look up users

--- main.sh
+++ main.sh
@@ -1,19 +1,19 @@
-# go run . reset
-# echo "======"
-# go run . register kahya
-# echo "======"
-# go run . addfeed "Hacker News RSS" "https://hnrss.org/newest"
-# echo "======"
-# go run . register holgith
-# echo "======"
-# go run . addfeed "Lanes Blog" "https://www.wagslane.dev/index.xml"
-# echo "======"
-# go run . follow "https://hnrss.org/newest"
-# echo "======"
-# go run . following
-# echo "======"
+go run . reset
+echo "======"
+go run . register kahya
+echo "======"
+go run . addfeed "Hacker News RSS" "https://hnrss.org/newest"
+echo "======"
+go run . register holgith
+echo "======"
+go run . addfeed "Lanes Blog" "https://www.wagslane.dev/index.xml"
+echo "======"
+go run . follow "https://hnrss.org/newest"
+echo "======"
+go run . following
+echo "======"
 # sleep 1
-# go run . login kahya
-# echo "======"
+go run . login kahya
+echo "======"
 # sleep 1
-# go run . following
+go run . following
---
+++ middleware.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"context"
+	"gator/internal/database"
+)
+
+func middlewareLoggedIn(handler func(s *state, cmd command, user database.User) error) func(*state, command) error {
+	return func(s *state, cmd command) error {
+		// 1. fetch the user (with error handling)
+		// 2. call handler(...) with the user passed in
+		user, err := s.db.GetUser(context.Background(), s.cfg.CurrentUserName)
+		if err != nil {
+			return err
+		}
+		return handler(s, cmd, user)
+	}
+}
